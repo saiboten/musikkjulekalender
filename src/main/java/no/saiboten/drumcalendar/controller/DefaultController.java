f916556e7681102d8cfc3255bbf0d031d863974a
saiboten <saiboten@gmail.com>
2016-10-09 23:00:01 +0200

    Added logout functionality

--- src/main/java/no/saiboten/drumcalendar/controller/DefaultController.java
+++ src/main/java/no/saiboten/drumcalendar/controller/DefaultController.java
@@ -1,5 +1,8 @@
 package no.saiboten.drumcalendar.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 import no.saiboten.drumcalendar.rest.RestController;
 
 import org.slf4j.Logger;
@@ -7,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class DefaultController {
@@ -24,6 +28,18 @@ public class DefaultController {
 		logger.debug("Request for front page");
 		return "main";
 	}
+	
+	@RequestMapping(value="/logout" )
+	public RedirectView secure(HttpServletRequest request) {
+		logger.debug("Request for front page");
+		try {
+			request.logout();
+		} catch (ServletException e) {
+			logger.error("Logout failed: "+e.getMessage());
+		}
+		return new RedirectView("/");
+	}
+	
 
 	@RequestMapping("/om")
 	public String om() {
