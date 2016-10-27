f3270104ff25572b60b0fbeb73c6c3a62e2f08a6
Tobias Rusås Olsen <saiboten@gmail.com>
2016-10-27 22:19:19 +0200

    Centered menu

--- src/main/resources/resources/static/react/components/Main.jsx
+++ src/main/resources/resources/static/react/components/Main.jsx
@@ -10,12 +10,16 @@ var Main = React.createClass({
     render: function() {
 
        var imgStyle = { width: "100%" };
+      var imgAttributes = {
+        src: "/static/images/santas.jpg",
+        alt: "Julenisse"
+      }
 
         return (
             <Block className="main-wrapper" backgroundColor="white" borderRadius="5px" margin="0 auto">
                 <Block>
-                    <Block component="h1" textAlign="center">Musikkjulekalender 2016!</Block>
-                    <img style={imgStyle} src="/static/images/santas.jpg" alt="Responsive image" />
+                    <Block padding-top="10px" component="h1" textAlign="center">Musikkjulekalender 2016!</Block>
+                    <Block width="100%" component="img" props={imgAttributes} />
                 </Block>
                 <SingleGuessDayContainer />
                 <UserStatisticsContainer />
